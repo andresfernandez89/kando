316ab83939a0021c26bb2dbd7d293718a7a2996d
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-06-03 22:19:30 -0300

    refactor: replace imports with aliases for clarity

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
-import User from "../../../../models/user.model";
+import { dbConnect } from "@/lib/dbConnect";
+import User from "@/models/user.model";
 import bcrypt from "bcryptjs";
-import { dbConnect } from "../../../../lib/dbConnect";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const { firstName, lastName, email, password, image } = await req.json();
